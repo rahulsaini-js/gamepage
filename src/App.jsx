a4bfaa288594a2de9933ee59af6c39c6033aea66
rahulsaini-js <itsrahulsaini21@gmail.com>
2024-11-27 11:29:23 +0530

    added story ection

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import Hero from "./components/Hero";
 import About from "./components/About";
 import NavBar from "./components/Navbar";
+import Features from "./components/Features";
+import Story from "./components/Story";
 
 const App = () => {
   return (
@@ -9,6 +11,8 @@ const App = () => {
       <NavBar />
       <Hero />
       <About />
+      <Features />
+      <Story />
     </main>
   );
 };
